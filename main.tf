31127c8e7f9055dfed249994776e2e2305000949
Shire <mohamoudabdiweli80@gmail.com>
2025-03-25 22:38:15 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-terraform-bucket-12345"
+
+  tags = {
+    Name        = "MyBucket"
+    Environment = "Dev"
+  }
+}
 
