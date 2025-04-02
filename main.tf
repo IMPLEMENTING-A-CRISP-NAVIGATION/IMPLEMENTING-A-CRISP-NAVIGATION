be626ff8e1c8b53dfd8e05a4e14dc5a735357a65
Shire <mohamoudabdiweli80@gmail.com>
2025-04-02 17:09:51 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1"  # Make sure the region matches your desired region
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "my-terraform-bucket-12345"
+  bucket = "my-terraform-bucket-unique-123456"  # Make sure to make this name unique
 
   tags = {
     Name        = "MyBucket"
     Environment = "Dev"
   }
 }
-
